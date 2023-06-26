cd8203443453bb81ef6eb1babe7d33e3472bc7e0
Atul kumar singh <aruatul8259@gmail.com>
2023-06-26 12:45:15 +0530

    Enviroments and gitignore

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt=require("mongoose-encryption");
 const app = express();
+
+console.log(process.env.API_KEYS);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -22,8 +25,7 @@ const userSchema =new mongoose.Schema( {
   password: String
 });
 
-const secret="hey whats u doin";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRETS,encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
